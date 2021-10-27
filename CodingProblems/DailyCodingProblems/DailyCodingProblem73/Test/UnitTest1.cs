7d729982c4c3553fbc2cc9827e20d90acc642120
Urlarrie <urlarrie@ncsu.edu>
2021-10-27 17:45:24 -0400

    Adjusting Repository

---
+++ CodingProblems/DailyCodingProblems/DailyCodingProblem73/Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DailyCodingProblem73
+{
+    /// <summary>
+    /// Tests the <see cref="/>
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
